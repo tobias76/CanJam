67239c3c567dbd447d0a5b8854c02fea007326d0
Toby <tobyreed0@gmail.com>
2016-03-20 04:10:01 +0000

    Zooming Camera Complete

--- CanJam16 Unity Project/Assets/Scripts/MainCamera.cs
+++ CanJam16 Unity Project/Assets/Scripts/MainCamera.cs
@@ -4,20 +4,36 @@ using System.Collections;
 public class MainCamera : MonoBehaviour
 {
 
+    public float zoomSpeed = 10;
+
+    public float smoothSpeed = 2;
+
+    public float targetOrtho;
+
 	// Use this for initialization
 	void Start ()
     {
-	
+        targetOrtho = Camera.main.orthographicSize;    
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-	
+        targetOrtho = Mathf.Clamp(targetOrtho, 3, 5);
+
+        scrollWheelControl();
 	}
 
     void scrollWheelControl()
     {
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
 
+        if (scroll != 0.0f)
+        { 
+                targetOrtho -= scroll * zoomSpeed;
+                targetOrtho = Mathf.Clamp(targetOrtho, 3, 5);
+            
+        }
+        Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, targetOrtho, smoothSpeed * Time.deltaTime);
     }
 }
