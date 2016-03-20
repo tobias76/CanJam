bee799814a72e08970a2592a511b1eff774b66a4
Toby <tobyreed0@gmail.com>
2016-03-20 00:29:52 +0000

    Added Oli's shit.
    Here are the menu bits.

---
+++ CanJam16 Unity Project/Assets/Menufunctions.js
@@ -0,0 +1,30 @@
+﻿var isQuit = false;
+
+function OnMouseEnter() {
+    //change text color
+    GetComponent.<Renderer>().material.color = Color.red;
+}
+
+function OnMouseExit() {
+    //change text color
+    GetComponent.<Renderer>().material.color = Color.white;
+}
+
+function OnMouseUp() {
+    //is this quit
+    if (isQuit == true) {
+        //quit the game
+        Application.Quit();
+    }
+    else {
+        //load level
+        Application.LoadLevel(1); //NEEDS REPLACING/updating 
+    }
+}
+
+function Update() {
+    //quit game if escape key is pressed
+    if (Input.GetKey(KeyCode.Escape)) {
+        Application.Quit();
+    }
+}
